9b68f53a2c4a63b69b72c66fd1ffdbe2e8259fc5
Arnav Sharma <143735941+iamarnav08@users.noreply.github.com>
2024-01-30 10:24:00 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('.topnav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-50px";
+//   }
+// }
