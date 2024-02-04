5d643ade13cb763965f28f7061fbfe1d62ddcde1
Arnav Sharma <143735941+iamarnav08@users.noreply.github.com>
2024-02-04 18:16:07 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -12,30 +12,6 @@ function dark_mode() {
     document.body.classList.toggle('dark_mode');
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    let commentsArray = [];
-    const likeCount = document.getElementById("likeCount");
-    let currentLikes = parseInt(localStorage.getItem("likeCount")) || 0;
-    likeCount.innerHTML = currentLikes;
 
-    function like() {
-        currentLikes++;
-        likeCount.innerHTML = currentLikes;
-        localStorage.setItem("likeCount", currentLikes);
-    }
-
-    function dislike() {
-        currentLikes--;
-        likeCount.innerHTML = currentLikes;
-        localStorage.setItem("likeCount", currentLikes);
-    }
-
-    const likeButton = document.getElementById("likeButton");
-    likeButton.addEventListener('click', like);
-
-    const dislikeButton = document.getElementById("dislikeButton");
-    dislikeButton.addEventListener('click', dislike);
-
-    
-});
+const pageIdentifier = 'blogPage1';
 
